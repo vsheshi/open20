9fe374e7a7697b1c5887a6a83c4ad408b6b57485
Domenico Cioffi <domenico.cioffi@lispa.it>
2019-01-15 08:55:21 +0100

    Add files via upload

---
+++ open20-application/src/main/php/open20/vendor/yurkinx/yii2-image/yii/image/drivers/Image/GD.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace yii\image\drivers;
+/**
+ * Support for image manipulation using [Imagick](http://php.net/Imagick).
+ *
+ * @package    Kohana/Image
+ * @category   Drivers
+ */
+class Image_GD extends Kohana_Image_GD {}
+
+?>
---
+++ open20-application/src/main/php/open20/vendor/yurkinx/yii2-image/yii/image/drivers/Image/Imagick.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yii\image\drivers;
+/**
+ * Support for image manipulation using [Imagick](http://php.net/Imagick).
+ *
+ * @package    Kohana/Image
+ * @category   Drivers
+ */
+class Image_Imagick extends Kohana_Image_Imagick {}
